3d295da40c24344cf5a9b6a01fc6b836f89bf858
Brendan Lawler <brendan.lawler@rackspace.co.uk>
2018-10-06 10:09:27 +0100

    Uploading

---
+++ List All VM's in RSG.ps1
@@ -0,0 +1,19 @@
+﻿        $rsglist = Get-AzureRmResourceGroup
+    foreach ($rsg in $rsglist)
+    {
+        $VMlist = Get-AzureRmVM -ResourceGroupName $rsg.ResourceGroupName
+        foreach ($VM in $vmlist)
+        {
+        Get-AzureRmResource -ResourceId $vm.NetworkProfile.NetworkInterfaces[0].Id | Get-AzureRmNetworkInterface | Select-Object -Property name, EnableAcceleratedNetworking
+    }
+    }
+
+    
+
+
+
+
+$vm = Get-Azurermvm -ResourceGroupName "RSGNAME" -Name "VMNAME"
+
+Get-AzureRmResource -ResourceId $vm.NetworkProfile.NetworkInterfaces[0].Id | Get-AzureRmNetworkInterface `
+| Select-Object -Property name, EnableAcceleratedNetworking
