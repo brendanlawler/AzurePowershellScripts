a7abbd28868413bed3130563c89bf1f52dca66a1
Brendan Lawler <brendan.lawler@rackspace.co.uk>
2018-10-11 17:42:50 +0100

    Update VM - 2xWindows2016+IIS.ps1

---
+++ VM - 2xWindows2016+IIS.ps1
@@ -0,0 +1,1 @@
+﻿ARMClient.exe delete /subscriptions/78a56ad7-5cea-4967-a51e-ddc8523ff5ea/resourceGroups/bldcrg1/providers/Microsoft.RecoveryServices/vaults/<recovery services vault name>?api-version=2015-03-15
