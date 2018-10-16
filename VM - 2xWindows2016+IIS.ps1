9f2b635bfb1071bca5aaae444e7959a43f17c86d
Brendan Lawler <brendan.lawler@rackspace.co.uk>
2018-10-16 15:57:25 +0100

    Update VM - 2xWindows2016+IIS.ps1

--- VM - 2xWindows2016+IIS.ps1
+++ VM - 2xWindows2016+IIS.ps1
@@ -1,1 +1,5 @@
-﻿ARMClient.exe delete /subscriptions/78a56ad7-5cea-4967-a51e-ddc8523ff5ea/resourceGroups/bldcrg1/providers/Microsoft.RecoveryServices/vaults/<recovery services vault name>?api-version=2015-03-15
+﻿$RSG-NET = WEU-RSG-NET
+$RSG-WEB = WEU-RSG-WEB
+
+
+New-Azure
